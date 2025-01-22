22d406a15673080cfeb128fce40d6f71dc90aee0
2Epik4u <justinabossmlg@gmail.com>
2025-01-22 11:28:15 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# cookie clicker ds
+very very very basic cookie clicker
+
+
+# how to play 
+tap the screen
+
+# how to make the game
+download devkitarm
+run make
---
+++ source/main.cpp
@@ -0,0 +1,60 @@
+/*---------------------------------------------------------------------------------
+
+	$Id: main.cpp,v 1.13 2008-12-02 20:21:20 dovoto Exp $
+
+	Simple console print demo
+	-- dovoto
+
+
+---------------------------------------------------------------------------------*/
+#include <nds.h>
+
+#include <stdio.h>
+
+static volatile int frame = 0;
+int cookies = 0;
+
+//---------------------------------------------------------------------------------
+// VBlank interrupt handler. This function is executed in IRQ mode - be careful!
+//---------------------------------------------------------------------------------
+static void Vblank() {
+//---------------------------------------------------------------------------------
+	frame++;
+}
+
+//---------------------------------------------------------------------------------
+int main(void) {
+//---------------------------------------------------------------------------------
+	touchPosition touchXY;
+
+	irqSet(IRQ_VBLANK, Vblank);
+
+	consoleDemoInit();
+
+	iprintf("      Hello DS dev'rs\n");
+	iprintf("     \x1b[32mwww.devkitpro.org\n");
+	iprintf("   \x1b[32;1mwww.drunkencoders.com\x1b[39m");
+
+	while(pmMainLoop()) {
+
+		swiWaitForVBlank();
+		scanKeys();
+		int keys = keysDown();
+		if (keys & KEY_START) break;
+		if (keys & KEY_TOUCH) {
+			cookies = cookies + 1;
+		}
+
+
+		touchRead(&touchXY);
+
+		// print at using ansi escape sequence \x1b[line;columnH
+		iprintf("\x1b[10;0HCookies Clicked = %d",cookies);
+
+		iprintf("\x1b[16;0HTouch x = %04X, %04X\n", touchXY.rawx, touchXY.px);
+		iprintf("Touch y = %04X, %04X\n", touchXY.rawy, touchXY.py);
+
+	}
+
+	return 0;
+}
